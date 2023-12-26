c446b5b03a82840b279d6d601114e613e0aee0ac
hieutran01 <trandinhhieu19012002@gmail.com>
2023-12-26 19:22:20 +0700

    seeder

---
+++ Modules/Employee/database/seeders/UserEmployeeSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Employee\database\seeders;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Database\Seeder;
+
+class UserEmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('user_employee')->insert([
+            [
+                'user_id' => 1,
+                'company_name' => 'Company ABC',
+                'company_website' => 'company_abc@example.com',
+                'company_phone' => '123-456-7890',
+                'company_address' => '123 Main St, City, Country',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
