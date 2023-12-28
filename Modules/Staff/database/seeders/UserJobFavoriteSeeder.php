04dd6718f352bf2dcd5c54237233dc4e0bd14106
hieutran01 <trandinhhieu19012002@gmail.com>
2023-12-28 23:35:27 +0700

    fix-sedd-UserJobFavorite

--- Modules/Staff/database/seeders/UserJobFavoriteSeeder.php
+++ Modules/Staff/database/seeders/UserJobFavoriteSeeder.php
@@ -10,13 +10,24 @@ class UserJobFavoriteSeeder extends Seeder
     /**
      * Run the database seeds.
      */
+    // public function run(): void
+    // {
+    //     DB::table('user_job_favorites')->insert([
+    //         'user_id' => 1, 
+    //         'job_id' => 1,
+    //         'created_at' => now(),
+    //         'updated_at' => now(),
+    //     ]);
+    // }
     public function run(): void
     {
-        DB::table('user_job_favorites')->insert([
-            'user_id' => 1, 
-            'job_id' => 1,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        for ($i = 1; $i <= 5; $i++) {
+            DB::table('user_job_favorites')->insert([
+                'user_id' => $i,
+                'job_id' => $i,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
