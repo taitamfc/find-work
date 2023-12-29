117d96f51bb066eb78f1e575a59427ffc2a8f141
hoangvanlong1999 <hoangvanlong.vn1999@gmail.com>
2023-12-29 21:19:56 +0700

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('website.dashboards.index');
+        return view('website.homes.index');
     }
 
     /**
