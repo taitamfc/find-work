f6f80c7776519303a26066c78859b1563f3d00e4
taifamfc <taitam8590@gmail.com>
2023-12-29 09:18:13 +0700

    update adminpost

--- app/Models/UserCV.php
+++ app/Models/UserCV.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserCV extends Model
+class UserCV extends AdminModel
 {
     use HasFactory;
     protected $table = 'user_cvs';
